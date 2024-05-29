4dcd634cc32391ca5e34832bafd4430c42473398
DavidTchintcharauli <chincharaulidavid@gmail.com>
2024-05-30 02:15:55 +0400

    add results colors

--- script.js
+++ script.js
@@ -8,20 +8,27 @@ function calculateBMI() {
         let bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2)
 
         let category = ''
+        let colorClass = ''
         if (bmi < 18.5) {
             category = 'Underweight'
+            colorClass = 'underweight'
         } else if (bmi >= 18.5 && bmi < 24.9){
             category = 'Normal weight'
+            colorClass = 'normal'
         } else if (bmi >= 25 && bmi < 29.9) {
             category = 'Overweight'
+            colorClass = 'overweight'
         } else {
             category = 'Obesity'
+            colorClass = 'obesity'
         }
 
         resultDiv.innerHTML = `Your BMI is ${bmi} (${category})`
+        resultDiv.className = 'result ' + colorClass
         resultDiv.style.opacity = 1
     } else {
         resultDiv.innerHTML = 'Pleasure Enter valid height and weight'
+        resultDiv.className = 'result'
         resultDiv.style.opacity = 1
     }
 }
